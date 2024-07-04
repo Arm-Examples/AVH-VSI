3d82f8cf43cfd60a014ae7bf65b2c4d3a7345166
Evagan2018 <42468904+Evagan2018@users.noreply.github.com>
2024-07-04 14:15:58 +0200

    Update main.c
    Test

--- hello_vsi/source/application/main.c
+++ hello_vsi/source/application/main.c
@@ -14,7 +14,7 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *---------------------------------------------------------------------------*/
+ *----------------------------------------------------------------------------*/
 
 #include "RTE_Components.h"
 #include CMSIS_device_header
